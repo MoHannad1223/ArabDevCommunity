8062f2326a3e0850fb930d4c6de9798e6715ec8c
Esraa <esraasherif1042@gmail.com>
2025-02-20 14:33:07 +0200

    USERPOSTNOT?

--- ArabDev.Data/DataOrEntities/Post.cs
+++ ArabDev.Data/DataOrEntities/Post.cs
@@ -25,7 +25,7 @@ namespace ArabDev.Data.DataOrEntities
 
         public User User { get; set; }
        
-        public int? UserId { get; set; }
+        public int UserId { get; set; }
 
         public ICollection<Likes> Likes { get; set; }=new List<Likes>();
         public ICollection<Comment> Comments { get; set; }=new List<Comment>();
